084f1a7dc655084d0523aa65dc3e3f2027628dfd
Abdullah Zia <abziawork960@gmail.com>
2024-08-04 23:05:10 +0500

    Chat UI

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,94 @@
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
+import red from "@mui/material/colors/red";
+
 const Chat = () => {
-  return <div>Chat</div>;
+  const auth = useAuth();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: {
+            md: "flex",
+            xs: "none",
+            sm: "none",
+            flex: 0.2,
+            flexDirection: "column",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to GPT
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask anything to GPT related to knowledge. It aint that
+            honest, but still gives answer xD
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", flex: { md: 0.8, xs: 1, sm: 1 } }}>
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+          }}
+        >
+          GPT MODEL - 3.5
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;
